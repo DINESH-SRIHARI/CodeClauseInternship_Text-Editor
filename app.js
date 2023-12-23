71d4dc97193c6f976078557d0ef7a55d4b2599ac
srihari dinesh <dineshdinny>
2023-12-23 15:03:58 +0530

    final version

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-const bodyParser = require('body-parser')
-const express=require('express')
-const app=express()
+const bodyParser = require('body-parser');
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
 app.use(express.static('public'))
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({exented:true}))
-app.get('/',(req,res)=>{
-    res.render('Welcome')
-})
-app.get('/doc',(req,res)=>{
-    res.render('home')
-})
-app.post('/download',async(req,res)=>{
-    const { htmlContent } = req.body;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+app.get('/', (req, res) => {
+    res.render('Welcome');
+});
 
-    // Your PDF generation logic goes here
-    await page.setContent(htmlContent);
-    const pdfBuffer = await page.pdf();
+app.get('/doc', (req, res) => {
+    res.render('home');
+});
 
-    await browser.close();
+app.post('/download', async (req, res) => {
+    try {
+        const { htmlContent } = req.body;
 
-    // Send the PDF as a download
-    res.setHeader('Content-Disposition', 'attachment; filename=document.pdf');
-    res.setHeader('Content-Type', 'application/pdf');
-    res.send(pdfBuffer);
-})
-app.listen('5000',()=>{
-    console.log('hi')
-})
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        // Your PDF generation logic goes here
+        await page.setContent(htmlContent);
+        const pdfBuffer = await page.pdf();
+
+        await browser.close();
+
+        // Send the PDF as a download
+        res.setHeader('Content-Disposition', 'attachment; filename=document.pdf');
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
